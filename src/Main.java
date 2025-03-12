21b177abdc842055f8c32f31b9718b00d0f6264b
KappaChungus <janek.code.master@gmail.com>
2025-03-12 17:23:44 +0100

    adjust to task requirements
    remove duplicated data stored in vectors
    
    adjust reading vectors test file, so program stores its suggested group
    
    print accuracy of the model instead of
    generating output file,

--- src/Main.java
+++ src/Main.java
@@ -6,53 +6,27 @@ import static java.lang.Math.sqrt;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Vector<Double>> preprocessedVectors = new ArrayList<>();
         int k = Integer.parseInt(args[0]);
-        HashMap<Vector<Double>, String> map = new HashMap<>();
-        ArrayList<Vector<Double>> newVectors = new ArrayList<>();
-        preprocess(args, map, preprocessedVectors, newVectors);
-        knn(preprocessedVectors,newVectors,map,k);
-        saveToFile(map);
+        HashMap<Vector<Double>, String> trainVectors = preprocess(args[1]);
+        HashMap<Vector<Double>, String> testVectors = preprocess(args[2]);;
+        System.out.println(knn(trainVectors,testVectors,k));
     }
 
-    public static void saveToFile(Map<Vector<Double>, String> map) {
-        Map<String, List<Vector<Double>>> groupedByValue = map.entrySet().stream()
-                .collect(Collectors.groupingBy(
-                        Map.Entry::getValue,
-                        TreeMap::new,
-                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())
-                ));
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
-            groupedByValue.entrySet().stream()
-                    .flatMap(entry -> entry.getValue().stream()
-                            .map(vector -> vector.stream()
-                                    .map(String::valueOf)
-                                    .collect(Collectors.joining(",")) + "," + entry.getKey()))
-                    .forEach(line -> {
-                        try {
-                            writer.write(line);
-                            writer.newLine();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void knn(ArrayList<Vector<Double>> preprocessedVectors,ArrayList<Vector<Double>> newVectors,HashMap<Vector<Double>, String> map, int k) {
-        for(Vector<Double> v: newVectors){
+    private static double knn(HashMap<Vector<Double>, String> trainVectors,HashMap<Vector<Double>, String> testVectors, int k) {
+        int res = 0;
+        for(Vector<Double> v: testVectors.keySet()) {
             kClosest closestVectors = new kClosest(k,v);
-            for(Vector<Double> w :preprocessedVectors){
+            for(Vector<Double> w :trainVectors.keySet()) {
                 closestVectors.add(w);
             }
             Set<Vector<Double>> closestSet = closestVectors.getClosestSet();
-            map.put(v,findDominant(closestSet,map));
+            if(findDominant(closestSet,trainVectors).equals(testVectors.get(v)))
+                res++;
         }
+        return (double) res /testVectors.size();
     }
 
-    public static String findDominant(Set<Vector<Double>> closestSet,HashMap<Vector<Double>, String> map) {
+    private static String findDominant(Set<Vector<Double>> closestSet,HashMap<Vector<Double>, String> map) {
         HashMap<String, Integer> frequencyMap = new HashMap<>();
         for (Vector<Double> v : closestSet) {
             String label = map.get(v);
@@ -65,18 +39,10 @@ public class Main {
                 .orElse(null);
     }
 
-    public static double dist(Vector<Double> v, Vector<Double> w) {
-        if (v.size() != w.size()) throw new RuntimeException("xd");
-        double res = 0;
-        for (int i = 0; i < v.size(); i++) {
-            res += Math.pow(v.get(i) - w.get(i), 2);
-        }
-        return sqrt(res);
-    }
-
-    public static void preprocess(String args[], HashMap<Vector<Double>, String> map, ArrayList<Vector<Double>> preprocessedVectors, ArrayList<Vector<Double>> newVectors) {
+    private static HashMap<Vector<Double>, String>  preprocess(String filepath) {
+        HashMap<Vector<Double>, String> map = new HashMap<>();
         try {
-            BufferedReader br = new BufferedReader(new FileReader(args[1]));
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
             String line;
             while ((line = br.readLine()) != null) {
                 String[] tokens = line.split(",");
@@ -88,19 +54,8 @@ public class Main {
                         map.put(vector, token);
                     }
                 }
-                preprocessedVectors.add(vector);
             }
-            br.close();
-            br = new BufferedReader((new FileReader(args[2])));
-            while ((line = br.readLine()) != null) {
-                String[] tokens = line.split(",");
-                Vector<Double> vector = new Vector<>();
-                for (String token : tokens) {
-                    vector.add(Double.parseDouble(token));
-                }
-                newVectors.add(vector);
-            }
-
+            return map;
         } catch (IOException e) {
             throw new RuntimeException("Error reading the file", e);
         }
--- src/kClosest.java
+++ src/kClosest.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
+import static java.lang.Math.sqrt;
+
 public class kClosest {
     private final int k;
     TreeSet<Vector<Double>> closestSet;
     Vector<Double> initial;
     kClosest(int k, Vector<Double> initial) {
         this.initial = initial;
-        closestSet = new TreeSet<>(Comparator.comparingDouble(v -> Main.dist(initial, v)));
+        closestSet = new TreeSet<>(Comparator.comparingDouble(v -> dist(initial, v)));
 
         this.k = k;
     }
@@ -15,6 +17,15 @@ public class kClosest {
         closestSet.add(v);
         if(closestSet.size()>k)
             closestSet.pollLast();
+    }
+
+    private static double dist(Vector<Double> v, Vector<Double> w) {
+        if (v.size() != w.size()) throw new RuntimeException("xd");
+        double res = 0;
+        for (int i = 0; i < v.size(); i++) {
+            res += Math.pow(v.get(i) - w.get(i), 2);
+        }
+        return sqrt(res);
     }
 
     public Set<Vector<Double>> getClosestSet() {
